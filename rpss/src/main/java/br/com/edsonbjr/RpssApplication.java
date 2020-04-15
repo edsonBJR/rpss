84f3d4dd9052c6e7f396d1aefb60dad2df557225
ebjr.tux <ebjr.tux@gmail.com>
2020-04-15 17:53:37 -0300

    Configuracao Inicial do Projeto

---
+++ rpss/src/main/java/br/com/edsonbjr/RpssApplication.java
@@ -0,0 +1,13 @@
+package br.com.edsonbjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpssApplication.class, args);
+	}
+
+}
---
+++ rpss/src/test/java/br/com/edsonbjr/RpssApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edsonbjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
